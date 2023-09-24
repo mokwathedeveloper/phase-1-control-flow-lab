ca13ad5833cd88fe1ed26eb20e39c075d40f3afd
MokwaTheDeveloper <moffatmokwa12@gmail.com>
2023-09-24 11:55:28 +0300

    Completed assgiment

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400) {
+    return "This one is on me!"
+  } else if (distance <= 2000) {
+    return "I will gladly take your thirty bucks."
+  } else {
+    return "No can do."
+} 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  if (city === "NYC") {
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(Tip){
+  if (Tip === "generous") {
+    return "Thank you so much."
+  }
 }
+
